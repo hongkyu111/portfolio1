b26405df4def3c2ba54e2c3327f8c77e498d2949
Hong Kyu Lee <83290263+hongkyu111@users.noreply.github.com>
2021-12-05 20:09:28 +0900

    SCSS upload
    SCSS upload

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,4 +1,4 @@
 import "../scss/styles.scss";
-alert("it works");
+console.log("main");
 
   
---
+++ src/client/js/videoPlayer.js
@@ -0,0 +1,123 @@
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const playBtnIcon = playBtn.querySelector("i");
+const muteBtn = document.getElementById("mute");
+const muteBtnIcon = muteBtn.querySelector("i");
+const volumeRange = document.getElementById("volume");
+const currenTime = document.getElementById("currenTime");
+const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
+const fullScreenBtn = document.getElementById("fullScreen");
+const fullScreenIcon = fullScreenBtn.querySelector("i");
+const videoContainer = document.getElementById("videoContainer");
+const videoControls = document.getElementById("videoControls");
+
+
+
+let controlsTimeout = null;
+let contorolsMovementTimeout = null;
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
+
+const handlePalyClick = (e) => {
+    if(video.paused){
+        video.play();
+    }else{
+        video.pause();
+    }
+    playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
+};
+
+const handleMuteClick = (e) => {
+    if(video.muted) {
+        video.muted = false;
+    } else {
+        video.muted = true;
+    }
+    muteBtnIcon.classList = video.muted
+    ? "fas fa-volume-mute"
+    : "fas fa-volume-up";
+    volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (event) => {
+    const {
+        target: {value},
+    } = event;
+    if(video.muted){
+        video.muted = false;
+        muteBtn.innerText = "Mute";
+    }
+    volumeValue = value;
+    video.volume = value;
+};
+
+const formatTime = (seconds) => 
+new Date(seconds * 1000).toISOString().substr(14, 5);
+
+
+const handleLoadedMetadata = () => {
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+    currenTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
+};
+
+const handleTimelineChange = (event) => {
+    const {
+        target: {value},
+    } = event;
+    video.currentTime = value;
+};
+
+const handleFullScreen = () => {
+    const fullscreen = document.fullscreenElement;
+    if(fullscreen) {
+     document.exitFullscreen();
+     fullScreenIcon.classList = "fas fa-expand";       
+    } else {
+     videoContainer.requestFullscreen();
+     fullScreenIcon.classList = "fas fa-compress";    
+    }
+};
+
+const hideControls = () => videoControls.classList.remove("showing");
+
+const handleMouseMove = () => {
+    if(controlsTimeout){
+        clearTimeout(controlsTimeout);
+        controlsTimeout = null;
+    }
+    if(contorolsMovementTimeout){
+        clearTimeout(contorolsMovementTimeout);
+        contorolsMovementTimeout = null;
+    }
+    videoControls.classList.add("showing");
+    contorolsMovementTimeout = setTimeout(hideControls,2000);
+};
+
+const handleMouseLeave = () => {
+    controlsTimeout = setTimeout(hideControls,2000);
+    
+};
+
+const handleSpaceBar = (event) => {
+    if(event.key === " "){
+        handlePalyClick();
+    }
+};
+
+playBtn.addEventListener("click", handlePalyClick);
+muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadeddata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
+videoContainer.addEventListener("mousemove", handleMouseMove);
+videoContainer.addEventListener("mouseleave", handleMouseLeave);
+timeline.addEventListener("input", handleTimelineChange);
+fullScreenBtn.addEventListener("click", handleFullScreen);
+document.addEventListener("keydown", handleSpaceBar);
